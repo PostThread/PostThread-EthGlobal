7476ffd1a77e8d49811f4b7db8f2b8eb0c6f81a0
grignaffini <grignaffinipaolo@gmail.com>
2022-05-19 22:39:53 +0200

    added auth

--- frontend/pages/authenticate.js
+++ frontend/pages/authenticate.js
@@ -1,16 +1,24 @@
 import React from 'react'
+import { useMoralis } from 'react-moralis';
+import { ConnectButton } from 'web3uikit';
 
 export default function Authenticate() {
+
+    const { account } = useMoralis()
 
     const loadWorldId = () => {
         worldID.init("world-id-container", {
             enableTelemetry: true,
-            actionId: "0x330C8452C879506f313D1565702560435b0fee4C", // <- use the address of your smart contract
-            signal: userWalletAddress, // <- Fill in with the user's wallet address here
+            actionId: contract_address, // <- use the address of your smart contract
+            signal: account, // <- Fill in with the user's wallet address here
         })
     };
 
     return (
-        <div id="world-id-container">World</div>
+        <>
+            <ConnectButton />
+            <div id="world-id-container">World</div>
+            <button onClick={loadWorldId()}>World Coin fire</button>
+        </>
     )
 }
